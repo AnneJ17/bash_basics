f99953594448cb0e2416e4a0e622d6c38dabbb58
Anne <ajoseph13@ggc.edu>
2020-02-03 17:11:34 -0500

    Completed the exercisein 03-happy.sh

--- 03-happy.sh
+++ 03-happy.sh
@@ -14,3 +14,12 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+
+echo "Is today the weekend?"
+read answer
+
+if [ "$answer" = "yes" ]; then
+   echo "Let's do some git branching."
+else
+   echo "Let's wait for the weekend to do some branching(git)"
+fi
